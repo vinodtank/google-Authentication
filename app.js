81ac48da954cb0096989bda48cdad3d89b9a7315
Vinod Tank <vinodtank@users.noreply.github.com>
2018-04-22 19:31:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,12 @@
 
 angular.module('myApp', [])
 .controller("loginController", ['$scope', function($scope) {
-
+  function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
 }]);
 })();
